9e5520d7183e4b06525cf1f7ad14e9f79076127c
BerniceMlangeni <bernicemlangeni@gmail.com>
2024-07-17 14:46:01 +0200

    Add project files.

---
+++ if-else-exercise/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace if_else_exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter a number");
+            number = Convert.ToInt32(Console.ReadLine());
+
+
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("Fizzbuzz!");
+            }
+            else if(number % 3 == 0)
+            {
+                Console.WriteLine("Fizz!");
+            }
+            else if(number % 5 == 0)
+            {
+                Console.WriteLine("Buzz!");   
+            }
+            else
+            {
+                Console.WriteLine("Otherwise"); 
+            }
+        }
+    }
+}
